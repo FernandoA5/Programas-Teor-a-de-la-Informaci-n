3970c8de7f4d6a893d9a40aeb5b7adcd71d91713
alcss <fernandoalcalacasas@hotmail.com>
2022-12-07 16:27:24 -0600

    Aún existe el problema del desplazamiento, definí el avance en 1, para ignorar el tamaño de la cadena encontrada y parece que el error de setear el último dígito desapareció, entonces parece que todo depende del contador de avance

--- Diccionario/src/main.rs
+++ Diccionario/src/main.rs
@@ -26,7 +26,6 @@ fn main() {
           let mut buffer: String = String::new();
           let mut buffer_aux: String= String::new();
           let mut ref_match: Referencia=Referencia{offset: 0,len: 0,character: '0'};
-          let mut ref_match_aux: Referencia=Referencia{offset: 0,len: 0,character: '0'};
           for i in contador_avance..VENTANA+contador_avance{ //HISTORIAL
                print!("[{}]", caracteres[i as usize]);
           }
@@ -40,7 +39,6 @@ fn main() {
                let mut cont_offset=0;
                for k in contador_avance..VENTANA+contador_avance{ //LOS COMPARAMOS CON EL HISTORIAL
                     cont_offset+=1;
-                    let mut contador_len=0;
                     if caracteres[j as usize]==caracteres[k as usize]{ //IF ENCUENTRA UNA COINCIDENCIA
                          //DEBEMOS RECORRER DESDE AQUÍ HACIA ADELANTE EN BUSCA DE MÁS COINCIDENCIAS PARA J
                          let mut buffer_aux:String = String::new();
@@ -48,8 +46,7 @@ fn main() {
                          for l in k..VENTANA+contador_avance{
                               let cond_iguales= caracteres[j_temp as usize] == caracteres[l as usize];
                               if cond_iguales{
-                                   buffer_aux.push(caracteres[j_temp as usize]);
-                                   contador_len+=1;
+                                   buffer_aux.push(caracteres[l as usize]);
                               }
                               j_temp+=1;
                               let cond_limite=j_temp-j+cont_offset >= FUTURO-2;
@@ -58,7 +55,7 @@ fn main() {
                                         buffer=buffer_aux.clone();
                                         ref_match=Referencia{
                                              offset:VENTANA-cont_offset+1, len: buffer.len() as i32,
-                                             character: caracteres[(j_temp-iteraciones) as usize]
+                                             character: caracteres[(l) as usize]
                                         };
                                    }
                                    break;
@@ -69,21 +66,22 @@ fn main() {
           }
           if buffer_aux.len()==0 && buffer.len() == 0{ //GUARDAMOS EL PRIMER CARACTER DEL LOOKAHEAD
                buffer_aux.push(caracteres[(contador_avance+VENTANA) as usize]); //GUARDAMOS EL CARACTER
-               ref_match_aux=Referencia{offset: 1, len: 1, character: caracteres[(contador_avance+VENTANA) as usize]
-               };
-               lista_referencias.push(ref_match_aux.clone());
+               lista_referencias.push(Referencia{offset: 1, len: 1, character: caracteres[(contador_avance+VENTANA) as usize]
+               });
                lista_textos.push(buffer_aux.clone());
+               contador_avance+=1;
           }else{
                lista_referencias.push(ref_match.clone());
-               contador_avance = if (contador_avance+(buffer.len()-1) as i32) <= caracteres.len() as i32{
-                    (contador_avance + (buffer.len() as usize) as i32) - iteraciones
+               contador_avance = if (contador_avance+(buffer.len()) as i32) <= caracteres.len() as i32{
+                    // (contador_avance + (buffer.len() as usize) as i32)
+                    contador_avance+1
                }else{break};
                lista_textos.push(buffer.clone());
           }
           println!("Ref: {:?}", lista_referencias);
           println!("texts: {:?}", lista_textos);
           print!("\n");
-          contador_avance+=1;
+          
           exit = if contador_avance+VENTANA+FUTURO == caracteres.len() as i32 { true } else { false };
           exit = if iteraciones==5 {true} else{false};
      }
